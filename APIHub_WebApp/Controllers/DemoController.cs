36d22b329177481cf1df02aa92e0c9c3dc025e5a
Muhammad Kamal <mahmoodkamaluddeen@gmail.com>
2023-12-25 15:08:59 +0100

    Added gitIgnore

--- APIHub_WebApp/Controllers/DemoController.cs
+++ APIHub_WebApp/Controllers/DemoController.cs
@@ -5,6 +5,7 @@ namespace APIHub_WebApp.Controllers
 {
     public class DemoController : Controller
     {
+
         public List<User> users = new List<User>()
          {
            new User(){UserId="202212",UserName="Bello.Musa", UserType="Admin", Password = "12345"},
@@ -19,8 +20,7 @@ namespace APIHub_WebApp.Controllers
             ViewBag.UserList = users;
             return View();
         }
-
-        
+    
         
         //Used ViewData to pass data from the controller to the view
         public IActionResult Index2()
@@ -29,6 +29,8 @@ namespace APIHub_WebApp.Controllers
             ViewData["UserList"]= users;
             return View();
         }
+
+        //---------------------------------------------------------------------------------------------------
 
 
         //TempData is used to move data from one Action method to another eg from TempData_Demo to TempData_Demo2
@@ -46,5 +48,43 @@ namespace APIHub_WebApp.Controllers
             TempData["Message2"] = TempData["Message"].ToString();
             return View();
         }
+
+        //---------------------------------------------------------------------------------------------------
+
+        //Using Session to pass data from one Action method to another
+        public IActionResult SessionLogin_Demo()
+        {
+            HttpContext.Session.SetString("username", "muhammad");
+            return RedirectToAction("SessionSuccess_Demo");
+
+        }
+
+        public IActionResult SessionSuccess_Demo()
+        {
+            ViewBag.Username = HttpContext.Session.GetString("username");
+            return View();
+        }
+
+
+        public IActionResult SessionLogout_Demo()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+
+
+        //---------------------------------------------------------------------------------------------------
+
+        //Passing of data using Query String, here you pass the parameter with the value in the URL by using ? and & to seperate the parameters. eg: http://localhost:5132/Demo/QueryString_Demo?name=kamal
+
+        public IActionResult QueryString_Demo()
+        {
+            string name = "Muhammad";
+            if(!string.IsNullOrEmpty(HttpContext.Request.Query["name"]))
+                name = HttpContext.Request.Query["name"];
+            ViewBag.Name = name;
+            return View();
+        }
+
     }
 }
--- APIHub_WebApp/Program.cs
+++ APIHub_WebApp/Program.cs
@@ -2,6 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(5);
+    options.Cookie.HttpOnly  = true;
+    options.Cookie.IsEssential = true;
+    
+});
 
 var app = builder.Build();
 
@@ -15,6 +22,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
